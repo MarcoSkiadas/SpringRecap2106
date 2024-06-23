e3283080f4ff913948339b4056ddc33fda08b793
MarcoSkiadas <Skiadas2@hotmail.com>
2024-06-23 12:01:35 +0200

    Initial commit

---
+++ src/main/java/org/example/springrecap2106/Service/RecapService.java
@@ -0,0 +1,7 @@
+package org.example.springrecap2106.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecapService {
+}
---
+++ src/test/java/org/example/springrecap2106/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springrecap2106;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
